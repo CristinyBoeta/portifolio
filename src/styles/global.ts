813ab687f2135624bdd8150ee9526956af0479ea
CristinyBoeta <cristinybt10@gmail.com>
2025-05-27 17:56:58 -0300

    styles: global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  
+  * {
+    text-decoration: none;
+    box-sizing: border-box;
+    margin: 0;
+  }
+  body, html {
+    padding: 0;
+    margin: 0;
+    font-size: 16px;
+    @media (max-height: 50.3125rem) {
+      font-size: 14px;
+    }
+
+    @media (max-height: 37.8125rem) {
+      font-size: 12px;
+    }
+    .grecaptcha-badge {
+      display: none !important;
+    }
+  }
+
+  h2{
+    font-size: 2.5rem;
+    margin: 1.5rem 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    @media (max-width: 50rem) {
+      font-size: 1.375rem;
+      margin: 1rem 0;
+    }
+  }
+
+  p{
+    font-weight: 400;
+    font-size: 0.875rem;
+    line-height: 1.5rem;
+  }
+  `;
+export default GlobalStyle;
